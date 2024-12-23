ab56299222cca0c3c42319bb27b1f6a4652f9d4a
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-12-23 19:50:43 +0700

    fix: authentik install script

--- scripts/generic/authentik/install.sh
+++ scripts/generic/authentik/install.sh
@@ -1,3 +1,4 @@
 helm repo add authentik https://charts.goauthentik.io
 helm repo update
 helm upgrade --install authentik authentik/authentik -f values.yaml -n authentik --create-namespace
+kubectl apply -f ingress.yaml -n authentik
