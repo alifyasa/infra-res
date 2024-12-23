2b850b6602104dff28c08ed1f633e756cad6b78e
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-12-24 04:55:14 +0700

    fix: improved traefik config

--- scripts/generic/k3s/install.sh
+++ scripts/generic/k3s/install.sh
@@ -3,3 +3,4 @@ curl -sfL https://get.k3s.io | sh -
 mkdir -p ~/.kube
 sudo chown $USER:$USER /etc/rancher/k3s/k3s.yaml
 sudo ln -s /etc/rancher/k3s/k3s.yaml ~/.kube/config
+kubectl apply -f traefik-config.yaml -n kube-system
