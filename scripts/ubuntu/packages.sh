bd17420a8be95559fe62a2c74dfc15ccbbefdefa
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-12-24 09:19:20 +0700

    feat: Add packages

--- scripts/ubuntu/packages.sh
+++ scripts/ubuntu/packages.sh
@@ -1,1 +1,1 @@
-sudo apt install build-essential jq wget curl tar gzip net-tools lsof traceroute nmap mtr whois fail2ban htop rsync zip unzip ripgrep fzf screen tmux aria2
+sudo apt install build-essential jq yq wget curl tar gzip net-tools lsof traceroute nmap mtr whois fail2ban htop rsync zip unzip ripgrep fzf screen tmux aria2
