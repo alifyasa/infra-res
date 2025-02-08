51404c566dec80d9736cc83a8eb74b0d4e6649ac
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2025-02-08 02:03:51 +0000

    fix: Longer timeout

--- manifests/syncthing/install.sh
+++ manifests/syncthing/install.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 IFS='\t\n'
 
 kubectl apply -f manifests/ns.yaml
-kubectl wait --for jsonpath='{.status.phase}=Active' --timeout=5s namespace/syncthing
+kubectl wait --for jsonpath='{.status.phase}=Active' --timeout=15s namespace/syncthing
 kubectl apply -f manifests
