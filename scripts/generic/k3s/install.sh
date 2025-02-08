19399a878543b6f1f27e624c64056ce600cbe5ac
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2025-02-08 00:13:51 +0000

    feat: Multi Master K3S

--- scripts/generic/k3s/install.sh
+++ scripts/generic/k3s/install.sh
@@ -1,5 +1,6 @@
 # Install k3s  
-curl -sfL https://get.k3s.io | sh -  
+curl -sfL https://get.k3s.io | sh -s server \
+  --cluster-init
 mkdir -p ~/.kube
 sudo chown $USER:$USER /etc/rancher/k3s/k3s.yaml
 sudo ln -s /etc/rancher/k3s/k3s.yaml ~/.kube/config
